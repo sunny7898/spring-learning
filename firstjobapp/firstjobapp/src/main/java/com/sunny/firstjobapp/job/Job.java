6a7ff4dcbda7f7dceeb7fa552963c3ded2d875c5
Sunny Goswami <sungoswami07gmail.com>
2024-03-06 21:15:14 +0530

    Spring learning - learning api build and its operations

---
+++ firstjobapp/firstjobapp/src/main/java/com/sunny/firstjobapp/job/Job.java
@@ -0,0 +1,67 @@
+package com.sunny.firstjobapp.job;
+
+public class Job {
+    private Long id;
+    private String title;
+    private String description;
+    private Long minSalary;
+    private Long maxSalary;
+    private String location;
+
+    public Job(Long id, String title, String description, Long minSalary, Long maxSalary, String location) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+        this.minSalary = minSalary;
+        this.maxSalary = maxSalary;
+        this.location = location;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Long getMinSalary() {
+        return minSalary;
+    }
+
+    public void setMinSalary(Long minSalary) {
+        this.minSalary = minSalary;
+    }
+
+    public Long getMaxSalary() {
+        return maxSalary;
+    }
+
+    public void setMaxSalary(Long maxSalary) {
+        this.maxSalary = maxSalary;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
---
+++ firstjobapp/firstjobapp/src/main/java/com/sunny/firstjobapp/job/impl/JobServiceImpl.java
@@ -0,0 +1,61 @@
+package com.sunny.firstjobapp.job.impl;
+
+import com.sunny.firstjobapp.job.Job;
+import com.sunny.firstjobapp.job.JobService;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+@Service
+public class JobServiceImpl implements JobService {
+
+    private List<Job> jobs = new ArrayList<>();
+
+    @Override
+    public List<Job> findAll() {
+        return jobs;
+    }
+
+    @Override
+    public void createJob(Job job) {
+        jobs.add(job);
+    }
+
+    @Override
+    public Job getJobById(Long id) {
+        for (Job job: jobs) {
+            if (job.getId().equals(id)) return job;
+        }
+        return null;
+    }
+
+    @Override
+    public boolean deleteJobById(Long id) {
+        Iterator<Job> iterator = jobs.iterator();
+        while (iterator.hasNext()) {
+            Job job = iterator.next();
+            if (job.getId().equals(id)){
+                iterator.remove();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public boolean updateJobById(Long id, Job updatedJob) {
+        for (Job job: jobs) {
+            if (job.getId().equals(id)){
+                job.setTitle(updatedJob.getTitle());
+                job.setDescription(updatedJob.getDescription());
+                job.setMaxSalary(updatedJob.getMaxSalary());
+                job.setMinSalary(updatedJob.getMinSalary());
+                job.setLocation(updatedJob.getLocation());
+                return true;
+            }
+        }
+        return false;
+    }
+}
